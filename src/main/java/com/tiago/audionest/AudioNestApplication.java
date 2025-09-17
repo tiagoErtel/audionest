73c7c0a29eabd8767bcc8f4bbf65b94e7c931e6c
Tiago Ertel <erteltiago0@gmail.com>
2025-09-17 15:03:05 +0100

    first commit

---
+++ src/main/java/com/tiago/audionest/AudioNestApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.audionest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiago/audionest/AudioNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.audionest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
